53ab2a36f376cba3d30e698af0ed5daf9b7a11dd
Katie Baynes <kathleen.baynes@nasa.gov>
2011-09-20 21:36:54 -0400

    first commit

---
+++ src/baynes/kathleen/contacts/Contacts605486Activity.java
@@ -0,0 +1,13 @@
+package baynes.kathleen.contacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Contacts605486Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

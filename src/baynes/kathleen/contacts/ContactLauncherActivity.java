7f8e97bb1c7b2b41543a6db16f561525c0375419
Katie Baynes <kathleen.baynes@nasa.gov>
2011-09-21 23:05:33 -0400

    nightly commit

--- src/baynes/kathleen/contacts/ContactLauncherActivity.java
+++ src/baynes/kathleen/contacts/ContactLauncherActivity.java
@@ -3,24 +3,38 @@ package baynes.kathleen.contacts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class ContactLauncherActivity extends Activity {
-    /** Called when the activity is first created. */
+    protected static final String TAG = "baynes.kathleen.contacts.ContactLauncherActivity";
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Button displayContact = (Button) findViewById(R.id.displayContact);
-        displayContact.setOnClickListener(new View.OnClickListener() {
+        
+        setButtonListener(R.id.editContact, "edit");
+        setButtonListener(R.id.displayContact, "display");
+    }
+
+	private void setButtonListener(int button_id, final String activity) {
+		Button button = (Button) findViewById(button_id);
+        button.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Intent display = new Intent();
-				
+				Log.d(TAG, "Click occurred for " + activity);
+				Intent intent = new Intent("baynes.kathleen.contacts." + activity);
+				//intent.putExtras(getExtrasForActivity(activity));
+				startActivity(intent);
 			}
 		});
-        		
-    }
+	}
+
+	protected Intent getExtrasForActivity(String activity) {
+		return null;
+	}
 }
--- src/baynes/kathleen/contacts/EditContactActivity.java
+++ src/baynes/kathleen/contacts/EditContactActivity.java
@@ -1,7 +1,13 @@
 package baynes.kathleen.contacts;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class EditContactActivity extends Activity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_contact);
+    }
 }

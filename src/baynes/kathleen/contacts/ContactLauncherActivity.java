9c4eda9784305b35b87432f7853b3ad843470351
Katie Baynes <kathleen.baynes@nasa.gov>
2011-10-10 20:31:54 -0400

    rearranging in preparation for next assignment

--- src/baynes/kathleen/contacts/ContactLauncherActivity.java
+++ src/baynes/kathleen/contacts/ContactLauncherActivity.java
@@ -1,24 +1,18 @@
 package baynes.kathleen.contacts;
 
 import baynes.kathleen.contacts.db.ContactsDB;
-import baynes.kathleen.contacts.models.Contact;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
 
 /**
  * This is the main Activity to be launched upon starting this application. It
@@ -40,48 +34,8 @@ public class ContactLauncherActivity extends Activity {
 	/** The Constant CONTACT, used for populating extras */
 	public static final String CONTACT_ID = "contact_id";
 
-	/** The contact list adapter. */
-	private ContactListAdapter contactListAdapter = null;
-
 	/** The contact database */
 	private ContactsDB contactsDB;
-	
-
-	/** The inflater. */
-	private LayoutInflater inflater;
-
-	/**
-	 * This class handles display of the contacts and populated the rows of the
-	 * ListView.
-	 */
-	private final class ContactListAdapter extends SimpleCursorAdapter {
-
-		public ContactListAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
-			super(context, layout, c, from, to);
-		}
-
-		@Override
-		public View getView(int position, View availableView, ViewGroup group) {
-
-			View row;
-
-			if (null == availableView) {
-				row = inflater.inflate(R.layout.contact_entry, null);
-			} else {
-				row = availableView;
-			}
-			TextView displayName = (TextView) row.findViewById(R.id.display_name);
-			Contact contact = contactsDB.retrieveContact(position + 1);
-			
-			if (contact != null) {
-				displayName.setText(contact.getDisplayName());
-				TextView phoneNumber = (TextView) row.findViewById(R.id.home_phone);
-				phoneNumber.setText("(" + contact.getHomePhone() + ")" + " [id:" + contact.getId() + "]");
-			}
-			return row;
-		}
-
-	}
 
 	/**
 	 * Called when the activity is first created.
@@ -132,16 +86,11 @@ public class ContactLauncherActivity extends Activity {
 	private void populateList() {
 		contactsDB = new ContactsDB(this);
 
-		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		ListView list = (ListView) findViewById(R.id.contact_list);
 
-		contactListAdapter = new ContactListAdapter(this, R.layout.contact_entry, contactsDB.getAllCursor(), new String[] {
-		    ContactsDB.DISPLAY_NAME, ContactsDB.HOME_PHONE }, new int[] { R.id.display_name_value, R.id.home_phone_value });
+		list.setAdapter(new SimpleCursorAdapter(this, R.layout.contact_entry, contactsDB.getAllCursor(), new String[] {
+				ContactsDB.DISPLAY_NAME, ContactsDB.HOME_PHONE }, new int[] { R.id.display_name, R.id.home_phone }));
 		
-		//this following line should manage the cursor related to the adapter. this should stop pesky stacktraces in the log
-		startManagingCursor(contactListAdapter.getCursor());
-		
-		list.setAdapter(contactListAdapter);
 		list.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
